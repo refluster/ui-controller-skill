b131784e7707139d7df842ee5ee6916cd3822cc8
refluster <refluster@gmail.com>
2017-07-06 08:02:10 +0900

    create dashboard and set link to hero-detail

--- web/public/my-app/src/app/app.module.ts
+++ web/public/my-app/src/app/app.module.ts
@@ -1,20 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+import { RouterModule }   from '@angular/router';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		HeroDetailComponent
+		HeroesComponent,
+		HeroDetailComponent,
+		DashboardComponent
 	],
 	imports: [
 		BrowserModule,
-		FormsModule
+		FormsModule,
+		RouterModule.forRoot([
+			{
+				path: 'heroes',
+				component: HeroesComponent
+			}, {
+				path: 'dashboard',
+				component: DashboardComponent
+			}, {
+				path: 'detail/:id',
+				component: HeroDetailComponent
+			}, {
+				path: '',
+				redirectTo: '/dashboard',
+				pathMatch: 'full'
+			}
+		])
+
 	],
-	providers: [],
+	providers: [
+		HeroService
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
