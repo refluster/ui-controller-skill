36d53fe19f2d0ebbf0b97ebd54d847464b1d890b
refluster <refluster@gmail.com>
2017-07-06 23:08:30 +0900

    refactor

--- web/public/my-app/src/app/websocket.component.ts
+++ web/public/my-app/src/app/websocket.component.ts
@@ -5,30 +5,24 @@ import { Router } from '@angular/router';
 @Component({
 	providers: [WebsocketService],
 	selector: 'websocket',
-	template: '<div>websocket.component</div><button (click)="onClick()">hoge</button>',
+	template: '',
 	styleUrls: [],
 })
+
 export class WebsocketComponent implements OnInit, OnDestroy {
 	connection;
-	data;
 
 	constructor(private websocketService: WebsocketService,
 				private router: Router ){}
 
 	onClick(){
-		this.websocketService.emit('on_name', this.data);
-		this.data = '';
 	}
 
 	ngOnInit() {
 		this.websocketService.connect('hoge=hoge');
 		this.connection = this.websocketService.on('pageset').subscribe(data => {
 			console.log('pageset called', data);
-			this.data = data;
-			this.router.navigateByUrl('/page2');
-		});
-		this.connection = this.websocketService.on('emit_name').subscribe(data => {
-			this.data = data;
+			this.router.navigateByUrl(data['page']);
 		});
 	}
 
