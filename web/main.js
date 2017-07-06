a54fb169db93533af4cb8485a1becb4a61eb28d8
refluster <refluster@gmail.com>
2017-07-06 19:18:31 +0900

    from controller to viewer via http-post and websocket

--- web/main.js
+++ web/main.js
@@ -2,6 +2,9 @@
 const express = require('express')
 const app = new (express)();
 const bodyParser = require('body-parser');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 const port = 8100;
@@ -11,6 +14,7 @@ app.use(express.static('public'));
 app.post('/test1', (req, res) => {
 	console.log('test1 post');
 	console.log(req.body);
+	io.emit('pageset', req.body);
 	res.header("Content-Type", "application/json; charset=utf-8");
 	res.send('[hoge]');
 });
@@ -26,11 +30,22 @@ app.get('/test2', (req, res) => {
 	res.send('');
 });
 
-app.listen(port, error => {
+io.on('connection', (socket) => {
+	let query       = socket.handshake.query;
+	let hoge        = query.hoge;
+
+	socket.on('disconnect', function(){} );
+
+	socket.on('on_name', ( data ) => {
+		console.log(data);
+		io.emit('emit_name', {data: data});
+	});
+});
+
+http.listen(port, (error) => {
 	if (error) {
 		console.error(error);
 	} else {
 		console.info('listen: ', port);
 	}
 });
-
