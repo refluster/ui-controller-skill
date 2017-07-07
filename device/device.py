05b27bbfd9e202465848bff68246ea5bbbba4b01
refluster <refluster@gmail.com>
2017-07-08 03:46:18 +0900

    light on/off module

--- device/device.py
+++ device/device.py
@@ -8,23 +8,29 @@ OUT_GPIO_PIN = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(OUT_GPIO_PIN, GPIO.OUT)
 
-def pushSwitch(pinNum):
-    print("pushSwitch ", pinNum)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pinNum, GPIO.OUT)
-    GPIO.cleanup()
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pinNum, GPIO.OUT)
-    GPIO.output(pinNum, GPIO.LOW)
-    time.sleep(0.3)
-    GPIO.output(pinNum, GPIO.HIGH)
-
 class light:
-    def on(self):
-        pushSwitch(17)
+    def pushSwitch(self, pinNum):
+        print("pushSwitch ", pinNum)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pinNum, GPIO.OUT)
+        GPIO.cleanup()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pinNum, GPIO.OUT)
+        GPIO.output(pinNum, GPIO.LOW)
+        time.sleep(0.3)
+        GPIO.output(pinNum, GPIO.HIGH)
 
-    def off(self):
-        pushSwitch(2)
+    def _on(self):
+        self.pushSwitch(17)
+
+    def _off(self):
+        self.pushSwitch(2)
+
+def on():
+    light()._on()
+
+def off():
+    light()._off()
 
 if __name__ == "__main__":
     l = light()
