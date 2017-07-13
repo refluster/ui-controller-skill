06e7c8dfd5449a6095e3cab8afd97d983d370846
refluster <refluster@gmail.com>
2017-07-13 19:27:10 +0900

    line push js

--- web/public/my-app/src/app/controller.component.ts
+++ web/public/my-app/src/app/controller.component.ts
@@ -10,7 +10,7 @@ import 'rxjs/add/operator/toPromise';
 export class ControllerComponent {
 	url = 'http://52.198.86.179:8100/test1';
 	devctrlUrl = 'http://52.198.86.179:8100/devctrl';
-	//url = 'http://localhost:8100/test1';
+	linePushUrl = 'http://52.198.86.179:8100/linepush';
 	pages = ['page1', 'page2', 'page3', 'page4']
 
 	constructor(private http: Http) { }
@@ -26,7 +26,16 @@ export class ControllerComponent {
 
 	ctrlLight(ctrl): Promise<void> {
 		return this.http.post(this.devctrlUrl, JSON.stringify({light: {cmd: ctrl, delay: 0}}),
-															  {headers: this.headers})
+							  {headers: this.headers})
+			.toPromise()
+			.then(() => {})
+			.catch(this.handleError);
+	}
+
+	sendToLine(msg): Promise<void> {
+		console.log(JSON.stringify({value1: msg}));
+		console.log(this.linePushUrl);
+		return this.http.post(this.linePushUrl, JSON.stringify({value1: msg}), {headers: this.headers})
 			.toPromise()
 			.then(() => {})
 			.catch(this.handleError);
