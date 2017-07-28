ab5ac970d39656776d6a872dba613c0dbf60da2f
refluster <refluster@gmail.com>
2017-07-28 14:41:45 +0900

    change display setting on div element at the trigger of websocket

--- site/src/app/viewer.component.ts
+++ site/src/app/viewer.component.ts
@@ -1,8 +1,10 @@
 import { Component, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { OnInit } from '@angular/core';
+import { WebsocketService }       from './websocket.service';
 
 @Component({
+	providers: [WebsocketService],
 	selector: 'viewer',
 	templateUrl: 'viewer.component.html',
 	styleUrls: ['./viewer.component.css'],
@@ -11,12 +13,28 @@ import { OnInit } from '@angular/core';
 export class ViewerComponent implements OnInit {
 	private _el: HTMLElement;
 	private dispVideoBorder: boolean = false;
+	private connection;
 
-	constructor(el: ElementRef) {
+	constructor(private websocketService: WebsocketService, el: ElementRef) {
 		this._el = el.nativeElement;
 	}
 
-	ngOnInit(): void {	}
+	ngOnInit(): void {
+		this.websocketService.connect('hoge=hoge');
+		this.connection = this.websocketService.on('message').subscribe(data => {
+			console.log(data);
+			if (data['movieset'] != undefined) {
+				let req = data['movieset']
+				console.log('movieset called', req);
+				this.movieSet(+req['movie']);
+			}
+		});
+		this.movieSet(1);
+	}
+
+	ngOnDestroy() {
+		this.connection.unsubscribe();
+	}
 
 	adjustPosition() {
 		let bb = this._el.getElementsByTagName('video')[0].getBoundingClientRect();
@@ -32,5 +50,14 @@ export class ViewerComponent implements OnInit {
 			video.style.border = '1px solid red';
 		}
 		this.dispVideoBorder = !this.dispVideoBorder
+	}
+
+	movieSet(number: number) {
+		console.log(number);
+		let m = this._el.getElementsByClassName('movie');
+		for (let i = 0; i < m.length; i++) {
+			(<HTMLElement>m[i]).style.display = 'none';
+		}
+		(<HTMLElement>m[number - 1]).style.display = 'block';
 	}
 }
