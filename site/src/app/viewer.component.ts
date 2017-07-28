092ef95777907f5396d58765c1e63b6c05c07b2c
refluster <refluster@gmail.com>
2017-07-28 15:41:27 +0900

    movie change without blockout

--- site/src/app/viewer.component.ts
+++ site/src/app/viewer.component.ts
@@ -17,6 +17,8 @@ export class ViewerComponent implements OnInit {
 	private countdownTimer;
 	private initialClock = new Date(1970, 0, 0, 0, 20, 35);
 	private clock_str = '';
+	private previousMovieNumber: number;
+	private currentMovieNumber: number;
 
 	constructor(private websocketService: WebsocketService, el: ElementRef) {
 		this._el = el.nativeElement;
@@ -32,6 +34,7 @@ export class ViewerComponent implements OnInit {
 				this.movieSet(+req['movie']);
 			}
 		});
+		this.currentMovieNumber = 2;
 		this.movieSet(1);
 	}
 
@@ -55,31 +58,43 @@ export class ViewerComponent implements OnInit {
 		this.dispVideoBorder = !this.dispVideoBorder
 	}
 
-	movieSet(number: number) {
+	movieChange(number: number) {
 		console.log(number);
 		let m = this._el.getElementsByClassName('movie');
-		for (let i = 0; i < m.length; i++) {
-			let v = (<HTMLElement>m[number - 1]).getElementsByTagName('video')[0];
-			v.currentTime = 0;
+		setTimeout(() => {
+			let _m = <HTMLElement>m[this.previousMovieNumber - 1];
+			let v = _m.getElementsByTagName('video')[0];
+			_m.style.display = 'none';
 			v.pause();
-			(<HTMLElement>m[i]).style.display = 'none';
-		}
+			v.currentTime = 0;
+		}, 100); // zantei duration for overlapping two movies
+
 		(<HTMLElement>m[number - 1]).style.display = 'block';
 		(<HTMLElement>m[number - 1]).getElementsByTagName('video')[0].play();
 
 		if (number == 3) {
 			this.countdownTimer = setInterval(() => {
 				this.initialClock.setSeconds(this.initialClock.getSeconds() - 1);
-				//this.clock = this.clock + 1;
 				this.clock_str = toStr(this.initialClock.getHours(), 2) + ':' +
 					toStr(this.initialClock.getMinutes(), 2) + ':' +
 					toStr(this.initialClock.getSeconds(), 2);
 			}, 1000);
 		} else {
-			if (this.countdownTimer !== undefined) {
-				this.countdownTimer.clearInterval();
+			if (this.countdownTimer != undefined) {
+				clearInterval(this.countdownTimer);
+				this.countdownTimer = undefined;
 			}
 		}
+	}
+
+	movieSet(number: number) {
+		if (this.currentMovieNumber == number) {
+			console.log('the same  number');
+			return;
+		}
+		this.movieChange(number);
+		this.previousMovieNumber = this.currentMovieNumber;
+		this.currentMovieNumber = number;
 	}
 }
 
