e5fff331c1bb60c3a8e8b11925b6a6504de45c29
refluster <refluster@gmail.com>
2017-08-03 22:40:14 +0900

    stop the small ball animation

--- site/src/app/viewer.component.ts
+++ site/src/app/viewer.component.ts
@@ -38,7 +38,7 @@ export class ViewerComponent implements OnInit {
 			}
 		});
 		this.currentMovieNumber = 2;
-		this.movieSet(1);
+		this.movieSet(3);
 
 		let weekName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 		let date = new Date();
@@ -122,30 +122,6 @@ export class ViewerComponent implements OnInit {
 		// show selected movie and play
 		(<HTMLElement>m[number - 1]).style.visibility = 'visible';
 		(<HTMLElement>m[number - 1]).getElementsByTagName('video')[0].play();
-
-		if (number == 3) {
-			/*
-			this.countdownTimer = setInterval(() => {
-				this.initialClock.setSeconds(this.initialClock.getSeconds() - 1);
-				this.clock_str = toStr(this.initialClock.getHours(), 2) + ' ' +
-					toStr(this.initialClock.getMinutes(), 2) + ' ' +
-					toStr(this.initialClock.getSeconds(), 2);
-			}, 1000);
-			*/
-			this.setSecondCount();
-			let clock = this._el.querySelector('#clock');
-			let ball = this._el.querySelector('#ball');
-			clock.classList.add('fadeIn');
-			ball.classList.add('fadeIn');
-		}
-		if (this.currentMovieNumber == 3) {
-			//clearInterval(this.countdownTimer);
-			this.setSecondCountStopFlag = false;
-			let clock = this._el.querySelector('#clock');
-			let ball = this._el.querySelector('#ball');
-			clock.classList.remove('fadeIn');
-			ball.classList.remove('fadeIn');
-		}
 	}
 
 	movieSet(number: number) {
