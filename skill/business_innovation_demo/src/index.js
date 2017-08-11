917745c12eaf1783c1f63c2131cd82220d2d6717
refluster <refluster@gmail.com>
2017-08-11 22:07:19 +0900

    skill for business innovation demo

---
+++ skill/business_innovation_demo/src/index.js
@@ -0,0 +1,85 @@
+'use strict';
+var Alexa = require("alexa-sdk");
+var http = require ('http');
+
+exports.handler = function(event, context, callback) {
+	var alexa = Alexa.handler(event, context);
+	alexa.registerHandlers(handlers);
+	alexa.execute();
+};
+
+var handlers = {
+	'LaunchRequest': function () {
+		console.log('============ [initial] ===========');
+		this.emit(':ask', 'This is home agent skill. May I help you?');
+		console.log('===== [initial] end');
+	},
+	'ControlDeviceIntent': function() {
+		let ctrl = this.event.request.intent.slots.ctrl.value;
+		let device = this.event.request.intent.slots.device.value.toLowerCase();
+		console.log('============ Control Device ===========');
+		console.log(' ctrl : ' + ctrl);
+		console.log(' device : ' + device);
+		switch (device) {
+		case 'panel light':
+			httppost('52.198.86.179', 8100, '/devctrl', {light: {cmd: ctrl, delay: 0}}, function() {
+				this.emit(':ask', 'Okay. Turning ' + ctrl + ' the panel light. Any request?');
+			}.bind(this));
+			break;
+		case 'tv':
+			httppost('52.198.86.179', 8100, '/devctrl', {tv: {cmd: ctrl, delay: 0}}, function() {
+				this.emit(':ask', 'Okay. Turning ' + ctrl + ' the tv. Any request?');
+			}.bind(this));
+			break;
+		case 'recorder':
+			httppost('52.198.86.179', 8100, '/devctrl', {recorder: {cmd: ctrl, delay: 0}}, function() {
+				this.emit(':ask', 'Okay. Turning ' + ctrl + ' the recorder. There are three titles. Since you are free until the 9 o\'clock, how about watching a movie?');
+			}.bind(this));
+			break;
+		default:
+			console.log('============ default ===========');
+			this.emit(':tell', 'Okay. Good bye.');
+			console.log('============ default ===========');
+		}
+		console.log('===== Control Device end');
+	},
+	'StopIntent': function() {
+		console.log('============ Stop ===========');
+		this.emit(':tell', 'Okay. Good bye.');
+		console.log('============ Stop End ===========');
+	},
+	'OkayIntent': function() {
+		console.log('============ Okay ===========');
+		this.emit(':tell', 'Okay. <prosody rate="x-slow"><amazon:effect name="whispered">setting the scene for horror movies. Enjoy.</amazon:effect></prosody>');
+		console.log('============ Okay End ===========');
+	},
+};
+
+function httppost(host, port, path, data, callback) {
+	let postDataStr = JSON.stringify(data);
+	let options = {
+		host: host,
+		port: port,
+		path: path,
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': postDataStr.length
+		}
+	};
+
+	let req = http.request(options, (res) => {
+		console.log('STATUS: ' + res.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(res.headers));
+		res.setEncoding('utf8');
+		res.on('data', (chunk) => {
+			console.log('BODY: ' + chunk);
+			callback();
+		});
+	});
+	req.on('error', (e) => {
+		console.log('problem with request: ' + e.message);
+	});
+	req.write(postDataStr);
+	req.end();
+}
