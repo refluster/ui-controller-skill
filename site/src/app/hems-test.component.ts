a3452b59fd13379acad4830cdc49fb44c1691b25
refluster <refluster@gmail.com>
2017-10-25 19:55:56 +0900

    refactor

--- site/src/app/hems-test.component.ts
+++ site/src/app/hems-test.component.ts
@@ -32,7 +32,7 @@ export class HemsTestComponent {
 
 	push() {
 		return this.http.post('https://api.push7.jp/api/v1/208062aac0394d13a3d454c445deb84d/send',
-							  '{"title": "スマートHEMS",  "body": "窓を閉め忘れていませんか？",  "icon": "http://is4.mzstatic.com/image/thumb/Purple118/v4/6e/f4/e5/6ef4e5fb-47dc-5c26-0a8b-67715f0f67ca/source/1200x630bb.jpg",  "url": "http://push7.jp/",  "apikey": "408b1365c3b94d98bc6dfe7cb54055e5"}',
+							  '{"title": "スマートHEMS",  "body": "窓を閉め忘れていませんか？",  "icon": "http://news.panasonic.com/jp/topics/items/NRR2015139937_1.pnghttp://is4.mzstatic.com/image/thumb/Purple118/v4/6e/f4/e5/6ef4e5fb-47dc-5c26-0a8b-67715f0f67ca/source/1200x630bb.jpg",  "url": "http://push7.jp/",  "apikey": "408b1365c3b94d98bc6dfe7cb54055e5"}',
 							  {headers: this.headers})
 			.toPromise()
 			.then(() => {})
@@ -53,7 +53,7 @@ export class HemsTestComponent {
 		window.addEventListener("devicemotion", handleMotion.bind(this), true);
 	}
 
-	test() {
+	windowNotify() {
 		function handleMotion(e) {
 			if (Math.abs(e.acceleration.x) > .5 ||
 				Math.abs(e.acceleration.y) > .5 ||
@@ -65,4 +65,5 @@ export class HemsTestComponent {
 		}
 		window.addEventListener("devicemotion", handleMotion.bind(this), true);
 	}
+
 }
